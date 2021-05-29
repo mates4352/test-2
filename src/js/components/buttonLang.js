079de75e27942ca53eec48809edc4af2e3f7be49
Sergey <cabyrkih@mail.ru>
2021-05-29 09:01:37 +0300

    btn lang

---
+++ src/js/components/buttonLang.js
@@ -0,0 +1,94 @@
+const buttonLang = () => {
+   const btnLang = document.querySelector('.main__lang')
+   const accordion = document.querySelector('.main__accordion')
+   const key = document.querySelectorAll('[data-key]')
+   const ea = document.querySelectorAll('.ea')
+   const ru = document.querySelectorAll('.ru')
+
+   const lang = {
+
+      ea: {
+         subtitle: '<span class="main__subtitle--block">Take a step</span> towards recovery',
+         labelName: 'Your <span class="main__label--green" >name</span>',
+         labelTel: 'Your phone <span class="main__label--green" >number</span>',
+         btn: 'To order',
+      },
+
+      ru: {
+         subtitle: '<span class="main__subtitle--block">Сделай шаг</span> к выздоровлению',
+         labelName: 'Ваше <span class="main__label--green" >имя</span>',
+         labelTel: 'Ваш <span class="main__label--green" >телефон</span>',
+         btn: 'Заказать',
+      }
+
+   }
+
+   btnLang.addEventListener('click', () => {
+
+      accordion.classList.toggle('active')
+
+      if (accordion.getAttribute('aria-hidden') === "true") {
+
+         btnLang.setAttribute('aria-label', 'закрыть изменение языка')
+         accordion.setAttribute('aria-hidden','false')
+
+      } else {
+
+         btnLang.setAttribute('aria-label','открыть изменение языка')
+         accordion.setAttribute('aria-hidden','true')
+
+      }
+
+   })
+
+   accordion.addEventListener('click', () => {
+
+      if (accordion.children[0].classList.contains('ea')) {
+
+         if ((accordion.getAttribute('id') === 'ea')) {
+
+            key.forEach((element) => {
+
+               const value = element.getAttribute('data-key')
+               console.log(value);
+               element.innerHTML = lang.ea[value]
+
+            });
+
+         }
+
+         for (let i = 0; i < ea.length; i++) {
+
+            accordion.setAttribute('id','ru')
+            btnLang.append(ea[i])
+            accordion.append(ru[i])
+
+         }
+
+      } else {
+
+         key.forEach((element) => {
+
+            const value = element.getAttribute('data-key')
+            console.log(value);
+            element.innerHTML = lang.ru[value]
+
+         });
+
+         for (let i = 0; i < ea.length; i++) {
+
+            accordion.setAttribute('id','ea')
+            accordion.append(ea[i])
+            btnLang.append(ru[i])
+
+         }
+
+      }
+
+      accordion.classList.remove('active')
+
+   })
+
+}
+
+export default buttonLang;
